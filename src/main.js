99694685b30cd3ab77bddcff49665212fb3ec586
Matej Bačo <matejbaco2000@gmail.com>
2023-09-14 17:54:05 +0200

    Fix dirname

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import satori from 'satori';
 import fs from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default async ({ req, res, log, error }) => {
   const fontSemibold = await fs.readFile(path.join(__dirname, '../fonts/Nunito-SemiBold.ttf'));
