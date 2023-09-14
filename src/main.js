29c9da41d1662c9c692fbd18329eb8aed7e85cb0
Matej Bačo <matejbaco2000@gmail.com>
2023-09-14 17:48:38 +0200

    Add basic OG image response

--- src/main.js
+++ src/main.js
@@ -1,33 +1,15 @@
-import { Client } from 'node-appwrite';
+import satori from 'satori';
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  const svg = await satori(
+    `<div style="color: black;">hello, world</div>`,
+    {
+      width: 1200,
+      height: 630
+    },
+  );
 
-  // You can log messages to the console
-  log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build Fast. Scale Big. All in One Place.',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
+  return res.send(svg, 200, {
+    'Content-Type': 'image/svg+xml',
   });
 };
