b31596987e11879f763e36ad7f6658986d88c34c
Matej Bačo <matejbaco2000@gmail.com>
2023-09-14 17:52:33 +0200

    Fix fonts

--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
 import satori from 'satori';
+import fs from 'fs/promises';
+import path from 'path';
 
 export default async ({ req, res, log, error }) => {
+  const fontSemibold = await fs.readFile(path.join(__dirname, '../fonts/Nunito-SemiBold.ttf'));
+  const fontBold = await fs.readFile(path.join(__dirname, '../fonts/Nunito-Bold.ttf'));
+
   const svg = await satori(
     `<div style="color: black;">hello, world</div>`,
     {
       width: 1200,
-      height: 630
+      height: 630,
+      fonts: [
+        {
+          name: 'Nunito',
+          data: fontSemibold,
+          weight: 600,
+          style: 'normal',
+        },
+        {
+          name: 'Nunito',
+          data: fontBold,
+          weight: 700,
+          style: 'normal',
+        },
+      ],
     },
   );
 
