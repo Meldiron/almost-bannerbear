41cc07e451334fc53d5c7278bc6f85d78ec01e16
Matej Bačo <matejbaco2000@gmail.com>
2023-09-14 18:10:00 +0200

    Update SVG

--- src/main.js
+++ src/main.js
@@ -7,38 +7,42 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default async ({ req, res, log, error }) => {
-  const fontSemibold = await fs.readFile(path.join(__dirname, '../fonts/Nunito-SemiBold.ttf'));
-  const fontBold = await fs.readFile(path.join(__dirname, '../fonts/Nunito-Bold.ttf'));
+    if(req.path === 'image') {
+    const fontSemibold = await fs.readFile(path.join(__dirname, '../fonts/Nunito-SemiBold.ttf'));
+    const fontBold = await fs.readFile(path.join(__dirname, '../fonts/Nunito-Bold.ttf'));
 
-  const svg = await satori(
-    {
-      type: 'div',
-      props: {
-        children: 'hello, world',
-        style: { color: 'black' },
+    const svg = await satori(
+      {
+        type: 'div',
+        props: {
+          children: 'hello, world',
+          style: { backgroundColor: 'black', color: 'white', width: '100vw', height: '100vh' },
+        },
       },
-    },
-    {
-      width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: 'Nunito',
-          data: fontSemibold,
-          weight: 600,
-          style: 'normal',
-        },
-        {
-          name: 'Nunito',
-          data: fontBold,
-          weight: 700,
-          style: 'normal',
-        },
-      ],
-    },
-  );
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: 'Nunito',
+            data: fontSemibold,
+            weight: 600,
+            style: 'normal',
+          },
+          {
+            name: 'Nunito',
+            data: fontBold,
+            weight: 700,
+            style: 'normal',
+          },
+        ],
+      },
+    );
 
-  return res.send(svg, 200, {
-    'Content-Type': 'image/svg+xml',
-  });
+    return res.send(svg, 200, {
+      'Content-Type': 'image/svg+xml',
+    });
+  }
+
+  return res.send("Use path /image");
 };
